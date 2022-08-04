c973b6f6b78d5c1135a07d9969848512242b91a3
Suk Hyun Moon <nuyhkusnoom@gmail.com>
2022-08-04 12:36:05 -0700

    Write run and mutate functions

--- src/ai/NeuralNetwork.js
+++ src/ai/NeuralNetwork.js
@@ -1,12 +1,17 @@
-import { inputLayer } from "@tensorflow/tfjs-layers/dist/exports_layers";
 // import {getState} from "../game/state";
 
+// // import {getState} from "../game/state";
+
 // let state = getState();
+
 
 // parameters
 let denselayersize = 10;
 let inputlayersize = 9;
 let outputlayersize = 3;
+let hardmutationrate = 0.10;
+let softmutationrate = 0.50;
+let mutationvariance = 0.05;
 
 // generates a fresh, randomized neural network
 function generate() {
@@ -106,7 +111,246 @@ function getRandom(min, max) {
 // if outcome was playerrock and computerrock, the inputlayer looks like [1, 0, 0, 0, 0, 0, 0, 0, 0]
 // if input layer is null (haven't played the first round yet, then computerselection is randomized
 function run(member, inputLayer) {
-    return "ROCK, PAPER, or SCISSOR";
+
+    // output = dotandadd(weights, biases, inputlayer)
+    let firstDenseLayer = dotandadd(member[0], member[1], inputLayer);
+
+    // ReLU activation (gets rid of negative values) and tanh to tame extreme values
+    firstDenseLayer = firstDenseLayer.map(x => Math.max(0, Math.tanh(x)));
+
+    // output = dotandadd(weights, biases, inputlayer)
+    let secondDenseLayer = dotandadd(member[2], member[3], firstDenseLayer);
+
+    // ReLU activation (gets rid of negative values) and tanh to tame extreme values
+    secondDenseLayer = secondDenseLayer.map(x => Math.max(0, Math.tanh(x)));
+
+    // output = dotandadd(weights, biases, inputlayer)
+    let outputLayer = dotandadd(member[4], member[5], secondDenseLayer);
+
+    // sigmoid the output layer (returns an array where the sum of all values equals 1)
+    outputLayer = outputLayer.map(x => sigmoid(x));
+    let sumNumber = outputLayer.reduce((a, b) => a + b, 0)
+    outputLayer = outputLayer.map(x => x/sumNumber);
+
+    return outputLayer;
 }
 
-export {generate, run};
+// dot product for each thing then sum then add bias
+function dotandadd (weights, biases, inputlayer) {
+
+    // output layer size is length of member 1, 3, 5 (biases)
+    // input layer size is length of inputlayer
+
+    // length of weights[0] = output layer size;
+    // length of weights = input layer size;
+
+    // I want to get final result of an outputlayer with length of outputlayersize
+
+    let outputlayerlength = biases.length;
+    let inputlayerlength = inputlayer.length;
+    let finalresult = [];
+
+    for (let n = 0; n < outputlayerlength; n++) {
+
+        let nodevalue = 0;
+
+        for (let v = 0; v < inputlayerlength; v++) {
+
+            nodevalue += weights[n][v] * inputlayer[v];
+        }
+        nodevalue += biases[n];
+    
+        finalresult.push(nodevalue);
+    }
+    return finalresult;
+}
+
+function sigmoid(x) {
+    return 1 / (1 + Math.exp(-x));
+}
+
+// possible hard mutate, then soft mutate based on variance
+function mutate(member) {
+
+    let layer1 = [];
+    for (let i = 0; i < member[0].length; i++) {
+        let thisnodeweights = [];
+        for (let w = 0; w < member[0][i].length; w++) {
+            thisnodeweights.push(member[0][i][w]);
+        }
+        layer1.push(thisnodeweights);
+    }
+
+    let layer2 = [];
+    for (let i = 0; i < member[1].length; i++) {
+        layer2.push(member[1][i])
+    }
+
+    let layer3 = [];
+    for (let i = 0; i < member[2].length; i++) {
+        let thisnodeweights = [];
+        for (let w = 0; w < member[2][i].length; w++) {
+            thisnodeweights.push(member[2][i][w]);
+        }
+        layer3.push(thisnodeweights);
+    }
+
+    let layer4 = [];
+    for (let i = 0; i < member[3].length; i++) {
+        layer4.push(member[3][i])
+    }
+
+    let layer5 = [];
+    for (let i = 0; i < member[4].length; i++) {
+        let thisnodeweights = [];
+        for (let w = 0; w < member[4][i].length; w++) {
+            thisnodeweights.push(member[4][i][w]);
+        }
+        layer5.push(thisnodeweights);
+    }
+
+    let layer6 = [];
+    for (let i = 0; i < member[5].length; i++) {
+        layer6.push(member[5][i])
+    }
+    
+    newmember = [];
+    let P;
+    let Q;
+
+    // first dense layer weights: member[0]
+    for (let i = 0; i < layer1.length; i++) {
+        for (let w = 0; w < layer1[i].length; w++) {
+            P = probabilityMatrix(layer1[i].length);
+            if (P[w] < hardmutationrate) {
+                layer1[i][w] = getRandom(-1,1);
+            }
+            Q = probabilityMatrix(layer1[i].length);
+            if (Q[w] < softmutationrate) {
+                layer1[i][w] *= getRandom(1-mutationvariance, 1+mutationvariance);
+            }
+            if (layer1[i][w] > 1) {
+                layer1[i][w] = 1;
+            }
+            if (layer1[i][w] < -1) {
+                layer1[i][w] = -1;
+            }
+        }
+    }
+    newmember.push(layer1);
+
+    // first dense layer biases: member[1]
+    P = probabilityMatrix(layer2.length);
+    Q = probabilityMatrix(layer2.length);
+    for (let i = 0; i < layer2.length; i++) {
+        if (P[i] < hardmutationrate) {
+            layer2[i] = getRandom(-1,1);
+        }
+        if (Q[i] < softmutationrate) {
+            layer2[i] *= getRandom(1-mutationvariance, 1+mutationvariance);
+        }
+        if (layer2[i] > 1) {
+            layer2[i] = 1;
+        }
+        if (layer2[i] < -1) {
+            layer2[i] = -1;
+        }
+    }
+    newmember.push(layer2);
+
+    // second dense layer weights: member[2]
+    for (let i = 0; i < layer3.length; i++) {
+        for (let w = 0; w < layer3[i].length; w++) {
+            P = probabilityMatrix(layer3[i].length);
+            if (P[w] < hardmutationrate) {
+                layer3[i][w] = getRandom(-1,1);
+            }
+            Q = probabilityMatrix(layer3[i].length);
+            if (Q[w] < softmutationrate) {
+                layer3[i][w] *= getRandom(1-mutationvariance, 1+mutationvariance);
+            }
+            if (layer3[i][w] > 1) {
+                layer3[i][w] = 1;
+            }
+            if (layer3[i][w] < -1) {
+                layer3[i][w] = -1;
+            }
+        }
+    }
+    newmember.push(layer3);
+
+    // second dense layer biases: member[3]
+    P = probabilityMatrix(layer4.length);
+    Q = probabilityMatrix(layer4.length);
+    for (let i = 0; i < layer4.length; i++) {
+        if (P[i] < hardmutationrate) {
+            layer4[i] = getRandom(-1,1);
+        }
+        if (Q[i] < softmutationrate) {
+            layer4[i] *= getRandom(1-mutationvariance, 1+mutationvariance);
+        }
+        if (layer4[i] > 1) {
+            layer4[i] = 1;
+        }
+        if (layer4[i] < -1) {
+            layer4[i] = -1;
+        }
+    }
+    newmember.push(layer4);
+
+    // output layer weights: member[4]
+    for (let i = 0; i < layer5.length; i++) {
+        for (let w = 0; w < layer5[i].length; w++) {
+            P = probabilityMatrix(layer5[i].length);
+            if (P[w] < hardmutationrate) {
+                layer5[i][w] = getRandom(-1,1);
+            }
+            Q = probabilityMatrix(layer5[i].length);
+            if (Q[w] < softmutationrate) {
+                layer5[i][w] *= getRandom(1-mutationvariance, 1+mutationvariance);
+            }
+            if (layer5[i][w] > 1) {
+                layer5[i][w] = 1;
+            }
+            if (layer5[i][w] < -1) {
+                layer5[i][w] = -1;
+            }
+        }
+    }
+    newmember.push(layer5);
+
+    // output layer biases: member[5]
+    P = probabilityMatrix(layer6.length);
+    Q = probabilityMatrix(layer6.length);
+    for (let i = 0; i < layer6.length; i++) {
+        if (P[i] < hardmutationrate) {
+            layer6[i] = getRandom(-1,1);
+        }
+        if (Q[i] < softmutationrate) {
+            layer6[i] *= getRandom(1-mutationvariance, 1+mutationvariance);
+        }
+        if (layer6[i] > 1) {
+            layer6[i] = 1;
+        }
+        if (layer6[i] < -1) {
+            layer6[i] = -1;
+        }
+    }
+    newmember.push(layer6);
+
+    return newmember;
+}
+
+function probabilityMatrix(length) {
+
+    let array = [];
+
+    for (let i = 0; i < length; i++) {
+        array.push(getRandom(0,1));
+    }
+
+    return array;
+}
+
+
+export {generate, run, mutate};
